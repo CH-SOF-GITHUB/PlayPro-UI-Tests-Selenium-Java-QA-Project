c6de0745380e78b1d77db3ea552d4c43f655ed69
CH-SOF-GITHUB <bchaker28@yahoo.com>
2025-12-29 16:22:25 +0100

    add first sample test with testingBot server cloud

--- src/test/java/config/ConfigTestingBot.java
+++ src/test/java/config/ConfigTestingBot.java
@@ -4,6 +4,8 @@ import com.testingbot.testingbotrest.TestingbotUnauthorizedException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
@@ -12,38 +14,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ConfigTestingBot {
-    // integrate TestingBot here
-    // "TestingBot Key = 5d2216ac43e908df72d5246c717d0715 | c17d99279baca27c772209c0ab678953
-    public static final String KEY = "f4f9a9f3e65be3bca6762f0521693de1";
-    // "TestingBot Secret = d05892a99eeb2bce005ff322fe1cba2b | 77f8f0a9ce9489c5ab1ac93fd4468f63"
-    public static final String SECRET = "027e95bda707ba75b14add8f3f0a8884";
+    // TestingBot key
+    public static final String KEY = "5c797835f6a08f70fd15b5823c2a6e52";
+    // TestingBot Secret
+    public static final String SECRET = "9fb978750fc0c45558d5caa85c9f239b";
     // Run cross browser Selenium tests on your local network or CI with TestingBot
-    public static String LOCAL_URL = "http://" + KEY + ":" + SECRET + "@localhost:4445/wd/hub";
+    // public static String LOCAL_URL = "http://" + KEY + ":" + SECRET + "@localhost:4445/wd/hub";
     // Run cross browser Selenium tests on TestingBot cloud platform
     public static String HUB_URL = "https://" + KEY + ":" + SECRET + "@hub.testingbot.com/wd/hub";
     // declare a remote web driver
-    protected RemoteWebDriver driver = null;
+    RemoteWebDriver driver = null;
 
     // ConfigTestingBot Constructor
     public ConfigTestingBot() {
     }
 
-    public RemoteWebDriver getTestingBotDriver() throws MalformedURLException, TestingbotUnauthorizedException {
-        System.out.println("----Driver is configured now !----\n");
-        ChromeOptions options = new ChromeOptions();
+    public RemoteWebDriver getTestingBotDriver() throws MalformedURLException {
+        try {
+            System.out.println("----Driver is configured now !----\n");
+            // set capabilities of tests
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability(CapabilityType.BROWSER_NAME, "MicrosoftEdge");
+            capabilities.setCapability(CapabilityType.BROWSER_VERSION, "latest");
+            capabilities.setCapability(CapabilityType.PLATFORM_NAME, "Windows 11");
 
-        Map<String, Object> testingBotOptions = new HashMap<>();
-        testingBotOptions.put("platform", "WIN10");
-        testingBotOptions.put("version", "latest");
-        testingBotOptions.put("build", "Build-TestingBot-Playpro-HUB-Grid-Automation - version 1.2");
-        testingBotOptions.put("tunnel", true);
-        testingBotOptions.put("screenshot", true);
-        options.setCapability("tb:options", testingBotOptions);
-
-        // initialise webdriver
-        driver = new RemoteWebDriver((new URL(HUB_URL)), options);
-
-        // return now the driver
-        return driver;
+            // capabilites specific to TestingBot
+            Map<String, Object> testingBotOptions = new HashMap<>();
+            testingBotOptions.put("build", "Build-TestingBot-Playpro-Demo");
+            testingBotOptions.put("tunnel", true);
+            testingBotOptions.put("screenshot", true);
+            capabilities.setCapability("tb:options", testingBotOptions);
+            // initialise webdriver
+            driver = new RemoteWebDriver((new URL(HUB_URL)), capabilities);
+            // return now the driver
+            return driver;
+        } catch (TestingbotUnauthorizedException e) {
+            throw e;
+        }
     }
 }
--- src/test/java/tests/PlayPro/DEV/Edge/TestingBot/SimpleTest.java
+++ src/test/java/tests/PlayPro/DEV/Edge/TestingBot/SimpleTest.java
@@ -1,10 +1,57 @@
 package tests.PlayPro.DEV.Edge.TestingBot;
 
+import com.testingbot.testingbotrest.TestingbotREST;
+import config.ConfigTestingBot;
+import config.LTConfigEdge;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class SimpleTest {
+import java.net.MalformedURLException;
+import java.rmi.Remote;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import static config.ConfigTestingBot.KEY;
+import static config.ConfigTestingBot.SECRET;
+
+
+public class SimpleTest extends ConfigTestingBot {
+    RemoteWebDriver driver = null;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+        System.out.println("Setup OK");
+        driver = getTestingBotDriver();
+    }
+
     @Test
     public void testMethod() {
+        driver.get("https://site.playpro.fr/");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(25));
+        WebElement ReservationsBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/header/div/div[1]/ul/a[6]")));
+        ReservationsBtn.click();
         System.out.println("Test OK");
+    }
+
+
+    @AfterTest
+    public void teardown() {
+        if (driver != null) {
+            TestingbotREST r = new TestingbotREST(KEY, SECRET);
+            Map<String, Object> data = new HashMap<>();
+            data.put("success", "1");
+            data.put("name", "Sample Selenium Test 1");
+            r.updateTest(driver.getSessionId().toString(), data);
+            driver.quit();
+            System.out.println("Teardown OK");
+        }
     }
 }
