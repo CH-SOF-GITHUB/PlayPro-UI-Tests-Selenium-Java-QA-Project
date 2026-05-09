ff7370d6b1a1152b25abbe68788089918008d73c
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-05-09 14:33:37 +0100

    add tests: navigate to reservations page

--- src/test/java/tests/PlayPro/DEV/BaseTest.java
+++ src/test/java/tests/PlayPro/DEV/BaseTest.java
@@ -1,5 +1,7 @@
 package tests.PlayPro.DEV;
 
+import PageObject.WebCookiesPage;
+import PageObject.WebLoginPage;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -25,7 +27,6 @@ public class BaseTest {
     Log log = LogFactory.getLog(BaseTest.class);
     // define explicit wait object
     protected WebDriverWait Wait;
-
     @BeforeMethod
     public void setUp() {
         log.info("Starting WebDriver...");
---
+++ src/test/java/tests/PlayPro/DEV/LoggedBaseTest.java
@@ -0,0 +1,36 @@
+package tests.PlayPro.DEV;
+
+import PageObject.WebCookiesPage;
+import PageObject.WebLoginPage;
+import org.testng.annotations.BeforeMethod;
+
+/*
+abstract empêche l'instanciation directe et signale l'intention « uniquement pour l'héritage ».
+TestNG ignore les classes abstraites pour la découverte d'exécutions, donc on évite que la classe soit traitée comme une suite de tests autonome.
+Les classes concrètes (vos tests) étendent cette classe pour bénéficier du @BeforeMethod d'auto-login sans dupliquer le code.
+Si vous préférez ne pas utiliser abstract, on peut aussi :
+rendre le constructeur protected pour limiter l'instanciation externe, ou
+garder la classe non-abstraite mais ne pas lui ajouter de @Test (TestNG n'exécutera rien tant qu'il n'y a pas de méthodes @Test dedans).
+La solution abstract reste la plus claire et sûre pour une classe utilitaire de setup partagé.
+ */
+public abstract class LoggedBaseTest extends BaseTest {
+    // Define email and password for login
+    protected final String user = "demotenant1@yopmail.com";
+    protected final String pass = "Admin1234!";
+    // define an instance of a specific page object
+    WebCookiesPage webCookiesPage = null;
+    WebLoginPage webLoginPage = null;
+
+    @BeforeMethod
+    public void loginBeforeEachTest() throws InterruptedException {
+        webCookiesPage = new WebCookiesPage(driver);
+        webLoginPage = new WebLoginPage(driver);
+        webCookiesPage.clickAcceptCookiesButton();
+        webLoginPage.EnterEmail(user);
+        webLoginPage.EnterPassword(pass);
+        webLoginPage.ClickLoginButton();
+
+        // small wait to ensure the login process is completed before proceeding with the tests
+        Thread.sleep(1500);
+    }
+}
