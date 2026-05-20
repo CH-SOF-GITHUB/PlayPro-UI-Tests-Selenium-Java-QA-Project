1fbf9977e843ade3c2e92f585c50aa0fb5a98e9d
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-05-20 20:09:00 +0100

    check tests js, webdriverIO, java

--- src/test/java/js/WebdriverIO/test/pageobjects/PlayPro/cookie.page.js
+++ src/test/java/js/WebdriverIO/test/pageobjects/PlayPro/cookie.page.js
@@ -9,8 +9,9 @@ class CookiePage extends Page {
     /**
      * define selectors using getter methods
      */
+
     get btnAccept() {
-        return $('button[data-testid="cookie-banner-accept-button"]');
+        return $('body > div.c15t-root-tnx7V.c15t-bottomLeft-wv7ez.c15t-bannerVisible-bty7Y > div > div > div.desktop-footer.desktop-footer.desktop-footer.c15t-footer-wS150 > div > button.c15t-button-YKOgW.c15t-button-small-n5LJg.c15t-button-primary-stroke-TWzjH');
     }
     /**
      * a method to encapsule automation code to interact with the page
--- src/test/java/js/WebdriverIO/test/pageobjects/PlayPro/register.page.js
+++ src/test/java/js/WebdriverIO/test/pageobjects/PlayPro/register.page.js
@@ -8,10 +8,13 @@ class RegisterPage extends Page {
     /**
      * define selectors using getter methods
      */
+    // div.w-3.h-3.rounded-full.bg-white
+    // html > body > div:nth-of-type(1) > main > div:nth-of-type(3) > form > div:nth-of-type(1) > div > label:nth-of-type(1)
     get checkMale() {
         return $('html > body > div:nth-of-type(1) > main > div:nth-of-type(3) > form > div:nth-of-type(1) > div > label:nth-of-type(1)');
     }
 
+    // ...
     get checkFemale() {
         return $('html > body > div:nth-of-type(1) > main > div:nth-of-type(3) > form > div:nth-of-type(1) > div > label:nth-of-type(2)');
     }
@@ -28,10 +31,13 @@ class RegisterPage extends Page {
         return $('input[placeholder=\'Adresse email*\']');
     }
 
+    // input.form-control
+    // input[type="tel"]
     get inputTelephone() {
-        return $('input[type="tel"]');
+        return $('input.form-control');
     }
 
+    // button.MuiButtonBase-root.MuiIconButton-root.MuiIconButton-edgeEnd.MuiIconButton-sizeMedium.css-slyssw
     get inputDateDeNaissance() {
         return $('input[value="DD/MM/YYYY"]');
     }
@@ -40,12 +46,20 @@ class RegisterPage extends Page {
         return $('input[placeholder=\'Mot de passe*\']');
     }
 
+    // div.absolute.shrink-0
+    // label.flex.items-start.gap-4.cursor-pointer.text-base.font-medium.overflow-auto
+    // div[class='relative w-full flex']
     get checkCGUV() {
-        return $('div[class=\'relative w-full flex\']');
+        return $('label.flex.items-start.gap-4.cursor-pointer.text-base.font-medium.overflow-auto');
     }
 
+    // button[class='w-full h-[45px] text-poppins text-medium text-[15px] leading-5 px-8 mb-6 border-black border rounded-[6px] bg-secondary text-accent']
+    // div[3] > form > button  ||   div[3]/form/button
+    // button[(text() = 'Créer mon compte' or . = 'Créer mon compte')]
+    // button[@type='button'])[7]
+    // body > div:nth-child(45) > main > div:nth-child(3) > form > button
     get btnRegister() {
-        return $('button[class=\'w-full h-[45px] text-poppins text-medium text-[15px] leading-5 px-8 mb-6 border-black border rounded-[6px] bg-secondary text-accent\']');
+        return $('button[class*=\'mb-6\']');
     }
 
     /**
@@ -66,30 +80,17 @@ class RegisterPage extends Page {
         await this.inputTelephone.setValue(telephone);
 
         // petit scroll manuel
-        await browser.execute(() => {
-            window.scrollBy(0, 300);
-        });
-
-        await browser.pause(1000);
-
+        await browser.pause(3000);
         await this.inputPassword.setValue(password);
 
         // scroll vers checkbox
-        await this.checkCGUV.scrollIntoView();
-
-        await browser.pause(1000);
-
+        await browser.pause(2000);
         await this.checkCGUV.click();
 
         // scroll vers bouton
-        await this.btnRegister.scrollIntoView();
-
-        await browser.pause(2000);
-
+        await browser.pause(3000);
         // JS click pour éviter "click intercepted"
-        await browser.execute((btn) => {
-            btn.click();
-        }, await this.btnRegister);
+        await this.btnRegister.click();
     }
 
     /**
