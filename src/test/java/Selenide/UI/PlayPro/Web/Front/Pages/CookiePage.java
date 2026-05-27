587ef841239460325b951620c0e575fe94cd8316
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-05-27 19:13:58 +0100

    run anf build selenide-playwright-selenium-java wrokflows

--- src/test/java/Selenide/UI/PlayPro/Web/Front/Pages/CookiePage.java
+++ src/test/java/Selenide/UI/PlayPro/Web/Front/Pages/CookiePage.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class CookiePage {
     // Define web elements using locators
     // SelenideElement acceptButton = $(By.xpath("//*[@data-testid = 'cookie-banner-footer-sub-group']//*[@data-testid = 'cookie-banner-accept-button']"));
-    private final SelenideElement acceptBtn = $(By.cssSelector("div[class='c15t-footerSubGroup-HbTp3'] button[class='c15t-button-YKOgW c15t-button-small-n5LJg c15t-button-primary-stroke-TWzjH']"));
+    private final SelenideElement acceptBtn = $(By.xpath("/html/body/div[3]/div/div/div[4]/div/button[1]"));
 
     // Define method to click on the accept button
     public void clickAcceptButton() {
