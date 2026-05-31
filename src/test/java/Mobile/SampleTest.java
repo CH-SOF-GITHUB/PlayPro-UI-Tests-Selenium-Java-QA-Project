9a1635f434528efe86309a1681a9155a7d035a48
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-05-31 18:23:41 +0100

    setup Appium mobile web testing with virtual android device

--- src/test/java/Mobile/SampleTest.java
+++ src/test/java/Mobile/SampleTest.java
@@ -1,10 +1,19 @@
 package Mobile;
 
 import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.time.Duration;
 
 public class SampleTest {
 
@@ -33,14 +42,22 @@ public class SampleTest {
         URL url = new URL("http://127.0.0.1:4723/");
 
         driver = new AndroidDriver(url, caps);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
 
     @Test
-    public void testGoogle() {
-
+    public void testGoogle() throws InterruptedException, IOException {
         driver.get("https://demotenant.playpro.fr/");
-
+        Thread.sleep(7000);
         System.out.println(driver.getTitle());
+        WebElement MenuBtn = driver.findElement(By.xpath("//button[@aria-label='Navigation.open_menu']"));
+        Assert.assertTrue(MenuBtn.isEnabled());
+        MenuBtn.click();
+        System.out.println("Menu button clicked successfully.");
+        // take the screenshot of the menu
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File file = ts.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File("src/test/java/Mobile/Screenshots/MenuScreenshot.png"));
     }
 
     @AfterTest
