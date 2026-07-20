b4b3826187dea1d2d6e7daa78d1dd753bb3f4751
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-07-20 01:06:57 +0100

    feat: add lambdatest-config file and run test for cloud browser execution

--- src/test/java/Cypress/cypress.config.js
+++ src/test/java/Cypress/cypress.config.js
@@ -1,11 +1,12 @@
-const { defineConfig } = require("cypress");
+const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        baseUrl: "https://chakerqa.playpro.fr",
+        specPattern: "cypress/e2e/**/*.cy.js",
+        supportFile: false
     },
-  },
 });
