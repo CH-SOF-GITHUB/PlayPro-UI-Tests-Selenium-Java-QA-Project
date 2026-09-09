d30353f1c78951bca8afd458656bb4fb99bab947
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-09-09 14:24:56 +0100

    (feat) - add getElementDescription method and display logs tests with description

--- src/main/java/org/qa/actionDriver/ActionDriver.java
+++ src/main/java/org/qa/actionDriver/ActionDriver.java
@@ -15,7 +15,7 @@ public class ActionDriver {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    public static final Logger loggr =  BaseClass.loggr;
+    public static final Logger loggr = BaseClass.loggr;
 
     // add a constructor of class
     public ActionDriver(WebDriver driver) {
@@ -27,10 +27,11 @@ public class ActionDriver {
 
     // Method to clic kin element
     public void click(By by) {
+        String elementDescription = getElementDescription(by);
         try {
             waitForElementToBeClickable(by);
             driver.findElement(by).click();
-            loggr.info("Clicked on element: {}", by.toString());
+            loggr.info("Clicked on element  ---> : {}", elementDescription);
         } catch (Exception e) {
             loggr.error("Element is not clickable: {}", e.getMessage());
         }
@@ -38,10 +39,11 @@ public class ActionDriver {
 
     // Method to enter text into input field
     public void enter(By by, String text) {
+        String elementDescription = getElementDescription(by);
         try {
             waitForElementToBeVisible(by);
             driver.findElement(by).sendKeys(text);
-            loggr.info("Value entered: {}", text);
+            loggr.info("Value entered on {} is  {}", elementDescription, text);
         } catch (Exception e) {
             loggr.error("Element is not entered: {}", e.getMessage());
         }
@@ -78,14 +80,15 @@ public class ActionDriver {
 
     // Method to check if an element is displayed
     public boolean isDisplayed(By by) {
+        String elementDescription = getElementDescription(by);
         try {
             waitForElementToBeVisible(by);
             boolean isDisplayed = driver.findElement(by).isDisplayed();
             if (isDisplayed) {
-                loggr.info("Element is displayed: {}", by.toString());
+                loggr.info("Element : {} is displayed", elementDescription);
                 return true;
             } else {
-                loggr.info("Element is not displayed: {}", by.toString());
+                loggr.info("Element : {} is not displayed", elementDescription);
                 return false;
             }
         } catch (Exception e) {
@@ -132,5 +135,57 @@ public class ActionDriver {
         } catch (Exception e) {
             loggr.error("This Element is not visible: {}", e.getMessage());
         }
+    }
+
+
+    // Method to get the description of an element using a locator
+    public String getElementDescription(By locator) {
+        // Check for null driver or locator to avoid NulPointer Exception
+        if (driver == null) {
+            return "driver is null";
+        }
+        if (locator == null) {
+            return "locator is null";
+        }
+        try {
+            // find the element using locator
+            WebElement element = driver.findElement(locator);
+
+            // get element attributes
+            String name = element.getDomAttribute("name");
+            String id = element.getDomAttribute("id");
+            String text = element.getText();
+            String className = element.getDomAttribute("class");
+            String placeHolder = element.getDomAttribute("placeholder");
+
+            // Return the description based on element attribute
+            if (isNotEmpty(name)) {
+                return "Element with name: " + name;
+            } else if (isNotEmpty(id)) {
+                return "Element with id: " + id;
+            } else if (isNotEmpty(text)) {
+                return "Element with text: " + truncate(text, 50);
+            } else if (isNotEmpty(className)) {
+                return "Element with class: " + className;
+            } else if (isNotEmpty(placeHolder)) {
+                return "Element with placeholder: " + placeHolder;
+            }
+        } catch (Exception e) {
+            loggr.error("Unable to describe the element: {}", e.getMessage());
+        }
+        return "Unable to describe the element";
+    }
+
+    // Utility method to check a string is not null or empty
+    private boolean isNotEmpty(String value) {
+        return value != null && !value.isEmpty();
+    }
+
+    // Utility Method to truncate long string
+    private String truncate(String value, int maxLength) {
+        if (value == null || value.length() <= maxLength) {
+            return value;
+        }
+        return value.substring(0, maxLength) + "...";
     }
 }
