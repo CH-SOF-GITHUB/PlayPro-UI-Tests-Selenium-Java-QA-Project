b678d43cb41d6a2dbf8ea553bea8460d973678b1
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-09-16 13:04:18 +0100

    (test)-BROWSER TESTING-ARGUMENTS A AJOUTER POUR FIXER L'ERREUR JENKINS

--- src/main/java/org/qa/base/BaseClass.java
+++ src/main/java/org/qa/base/BaseClass.java
@@ -128,6 +128,8 @@ public class BaseClass {
                         options.addArguments("--disable-notifications");
                         options.addArguments("--no-sandbox");
                         options.addArguments("--disable-dev-shm-usage");
+                        // ARGUMENTS A AJOUTER POUR FIXER L'ERREUR JENKINS :
+                        options.addArguments("--remote-debugging-port=9222");
                     }
                     driver.set(new EdgeDriver(options));
                     ExtentManager.registerDriver(getDriver());
